4f47c015c52dc6086fb1a5ac8ea5406f5268f5ca
Dhiraj kumar <kumardhiraj0248@gmail.com>
2023-05-22 19:21:48 +0530

    basic node js practice

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    {name:"dhiraj",email:"kumardhiraj0248@gmail.com",gender:"male"},
+    {name:"suraj",email:"suraj@gmail.com",contact:"9778534"},
+    {name:"dheeraj" ,email:"dheeraj@gmail.com",gender:"male"}
+]
+module.exports = data;
