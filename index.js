861c116fe5e676cee33cb3b96eb765e78e6c30fd
Dhiraj kumar <kumardhiraj0248@gmail.com>
2023-06-04 01:23:09 +0530

    image file uploded using multer packages

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
-//post api with mongoose
+/*//file upload using multer packeges first method
 const express = require("express");
-require("./config");
-const product = require("./product");
+const multer = require("multer");
 
 const app = express();
-app.use(express.json());
-//search using regex
-app.get("/search/:key",async(req,res)=>{
-  console.log(req.params.key);
-  const data = await product.find({
-    "$or":[
-      {"name":{$regex:req.params.key}},
-      {"brand":{$regex:req.params.key}},
-      {"category":{$regex:req.params.key}},
-    ]
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // Set the destination folder where files will be stored
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    // Set the filename for the uploaded file
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+app.post('/upload', upload.single('file'), (req, res) => {
+  // Access the uploaded file using req.file
+  // Process the file as needed
+  res.send('File uploaded successfully!');
+});
+
+
+app.listen(5000,()=>{
+  console.log("running on port 5000");
+})
+*/
+
+//file uploded using 2nd method using multer
+const express = require("express");
+const multer = require("multer");
+
+const app = express();
+
+const upload = multer({
+  storage:multer.diskStorage({
+    destination:function(req,file,cb){
+      cb(null,"uploads");
+    },
+    filename : function(req,file,cb){
+      cb(null,file.fieldname + "-" + Date.now()+".jpg");
+    }
   })
-  
-  res.send(data);
+}).single("user_file");
+
+app.post("/upload",upload,(req,res)=>{
+  res.send("uploaded successfully");
 })
 
-app.listen("5000",()=>{
-  console.log("port running on 5000");
-})
+app.listen(5000,()=>{
+  console.log("server up on 5000");
+});
